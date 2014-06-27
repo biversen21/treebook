e69dd6dcf836ea9599a82263631e2fd68865d954
Brian Iversen <biversen21@gmail.com>
2014-06-27 13:52:15 -0700

    Fixed Ruby 4 conflicts & added user name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,16 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
-end
+  before_filter :update_sanitized_params, if: :devise_controller?
+
+     protect_from_forgery with: :exception 
+
+  end
+
+  def update_sanitized_params
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:profile_name, 
+      :email, :first_name, :last_name, :password) }
+  end
+  
+  private
+  def status_params
+        params.require(:status).permit(:user_id, :content, document_attributes: [:attachment, :remove_attachment] ) if params[:status]
+      end
